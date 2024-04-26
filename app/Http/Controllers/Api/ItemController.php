f34a5d7f1abf43adec3197f09e098f9593e49d72
Genye Emeka <gilbertgenye4@gmail.com>
2024-04-26 16:19:45 +0100

    modified getItems route to return CategoryName

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -26,8 +26,18 @@ class ItemController extends Controller
             // Incase you want to display all items including the archived ones,
             // then comment the  code directly below and uncomment the following one
     
-            $items = Item::where('user_id', $user->id)->where('archived', false)->select(['title', 'subtitle', 'created_at', 'id'])->get();
-           // $items = Item::where('user_id', $user->id)->select(['title', 'subtitle', 'created_at', 'id'])->get();
+            // $items = Item::where('user_id', $user->id)->with('category')->where('archived', false)->select(['title', 'subtitle', 'created_at', 'id'])->get();
+            // $items = Item::where('user_id', $user->id)->with('category')->where('archived', false)->get();
+            
+            $items = Item::where('user_id', $user->id)
+                    ->with(['category' => function ($query) {
+                        $query->select('id', 'name');
+                    }])
+                    ->where('archived', false)
+                    ->get(['title', 'subtitle', 'created_at', 'id', 'category_id']);
+            
+           
+            // $items = Item::where('user_id', $user->id)->select(['title', 'subtitle', 'created_at', 'id'])->get();
             return response()->json($items, 200);
         
         } catch(\Throwable $throwable) {
@@ -276,7 +286,7 @@ class ItemController extends Controller
             ]);
 
 
-            return response()->json([$item], 200);
+            return response()->json($item, 200);
 
         } catch (\Throwable $throwable) {
             $message = $throwable->getMessage();
@@ -401,7 +411,7 @@ class ItemController extends Controller
             }
 
             $items = Item::where('user_id', Auth::user()->id)
-                ->select(['title', 'description', 'created_at', 'price'])
+                ->select(['id','title', 'description', 'created_at', 'price'])
                 ->orderBy('price', 'desc')
                 ->get();
     

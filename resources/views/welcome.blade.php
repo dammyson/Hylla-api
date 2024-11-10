98c152cd06f85b8a3496d69428ff8e43c4ba138a
Ayobami Ayeni <ayobami@Ayobamis-MacBook-Pro.local>
2024-11-10 14:52:20 -0600

    updat to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>HYLLA</title>
+        <title>HYLLA API</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">

7fc916878773da92caf2585b639d547b7cf7a5f1
Ayobami Ayeni <ayobami@Ayobamis-MacBook-Pro.local>
2024-11-10 15:23:58 -0600

    update to automatic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>HYLLA API TEST AND SEE</title>
+        <title>APP-HYLLA</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">

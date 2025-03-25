51281d5022cf32eec49a52ceba227c86dba3cbf8
Emeka-techDev <gilbertgenye@gmail.com>
2025-03-25 14:33:28 +0100

    image upload

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements FilamentUser
 {
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->is_admin ==1;
+        return str_ends_with($this->email, 'bami21@gmail.com');
     }
 
 
